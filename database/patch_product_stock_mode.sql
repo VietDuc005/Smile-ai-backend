48e83f2f4568c750265d36a3bdf08d746dedc6e3
VietDuc005 <ducsmile1111@gmail.com>
2026-04-23 19:36:26 +0700

    Delete database directory

--- database/patch_product_stock_mode.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Products
-    ADD COLUMN requires_inventory BOOLEAN NOT NULL DEFAULT TRUE AFTER image_url,
-    ADD COLUMN stock_quantity INT NOT NULL DEFAULT 0 AFTER requires_inventory;
